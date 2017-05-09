6b91c29999867eccc585285240c20cc91e47bfeb
Diego Staphorst <diego-s-@hotmail.com>
2017-05-09 15:18:47 +0200

    Charts fuel, temperture and VA

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,29 +1,9 @@
 (function () {
     let chartCollection = {
-        lineDataVA: {
+        'linechart-va-data': {
             labels: [],
             datasets: [{
-                label: "VA generator 1",
-                backgroundColor: "rgba(135, 211, 124, .3)",
-                borderColor: "rgba(75,192,192,1)",
-                pointBorderColor: "rgba(75,192,192,1)",
-                pointBackgroundColor: "#fff",
-                pointRadius: 3,
-                pointHitRadius: 10,
-                data: []
-            },
-            {
-                label: "Temperture generator 1",
-                backgroundColor: "rgba(135, 211, 124, .3)",
-                borderColor: "rgba(75,192,192,1)",
-                pointBorderColor: "rgba(75,192,192,1)",
-                pointBackgroundColor: "#fff",
-                pointRadius: 3,
-                pointHitRadius: 10,
-                data: []
-            },
-            {
-                label: "Fuel amount generator 1",
+                label: "VA, generator 1",
                 backgroundColor: "rgba(135, 211, 124, .3)",
                 borderColor: "rgba(75,192,192,1)",
                 pointBorderColor: "rgba(75,192,192,1)",
@@ -33,7 +13,34 @@
                 data: []
             }]
         },
-        lineOptions: { // Styling for charts
+        'linechart-temp-data': {
+            labels: [],
+            datasets: [{
+                label: "Temperture, generator 1",
+                backgroundColor: "rgba(135, 211, 124, .3)",
+                borderColor: "rgba(75,192,192,1)",
+                pointBorderColor: "rgba(75,192,192,1)",
+                pointBackgroundColor: "#fff",
+                pointRadius: 3,
+                pointHitRadius: 10,
+                data: []
+            }]
+        },
+        'linechart-fuel-data': {
+            labels: [],
+            datasets: [{
+                label: "Fuel, generator 1",
+                backgroundColor: "rgba(135, 211, 124, .3)",
+                borderColor: "rgba(75,192,192,1)",
+                pointBorderColor: "rgba(75,192,192,1)",
+                pointBackgroundColor: "#fff",
+                pointRadius: 3,
+                pointHitRadius: 10,
+                data: []
+            }]
+
+        },
+        'lineOptions': { // Styling for charts
             legend: {
                 labels: {
                     fontColor: "white",
@@ -66,19 +73,27 @@
         render: function () {
             const charts = Array.from(document.getElementsByTagName('canvas'));
             charts.map((chart) => {
-                chartCollection.lineChart = Chart.Line(document.getElementById(chart.id).getContext('2d'), {
-                    data: chartCollection.lineData,
+                console.log(chart.id)
+                chartCollection[chart.id] = Chart.Line(document.getElementById(chart.id).getContext('2d'), {
+                    data: chartCollection[`${chart.id}-data`],
                     // options: chartCollection.lineOptions
                 })
             })
         },
         updateData: function (zone, measurement) {
             let measurementArray = measurement.split(',')
-            let date = new Date(Number(measurementArray[0])*1000);
+            let date = new Date(Number(measurementArray[0]) * 1000);
             let time = `${date.getHours()}:${('0'+date.getMinutes()).slice(-2)}`
-            chartCollection.lineChart.data.datasets[zone-1].data.push(measurementArray[2])
-            chartCollection.lineChart.data.labels.push(time);
-            chartCollection.lineChart.update();
+            console.log(chartCollection['linechart-va'])
+            chartCollection['linechart-va'].data.datasets[zone - 1].data.push(measurementArray[1])
+            chartCollection['linechart-va'].data.labels.push(time);
+            chartCollection['linechart-temp'].data.datasets[zone - 1].data.push(measurementArray[2])
+            chartCollection['linechart-temp'].data.labels.push(time);
+            chartCollection['linechart-fuel'].data.datasets[zone - 1].data.push(measurementArray[3])
+            chartCollection['linechart-fuel'].data.labels.push(time);
+            chartCollection['linechart-va'].update();
+            chartCollection['linechart-temp'].update();
+            chartCollection['linechart-fuel'].update();
         }
     }
     chartCollection.render();
