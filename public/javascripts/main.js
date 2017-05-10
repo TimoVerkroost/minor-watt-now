6b8412e57c066e2d7cd626b17d80bc9dd4da54f8
ColinDorr <colin.maloud@gmail.com>
2017-05-10 12:16:15 +0200

    generator selector

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -43,29 +43,29 @@
         'lineOptions': { // Styling for charts
             legend: {
                 labels: {
-                    fontColor: "white",
+                    // fontColor: "white",
                     fontSize: 12
                 }
             },
             scales: {
                 yAxes: [{
-                    stacked: true,
+                    stacked: false,
                     gridLines: {
                         display: true, //Show gridlines
-                        color: "rgba(255,255,255,.5)" //color gridlines
+                        // color: "rgba(255,255,255,.5)" //color gridlines
                     },
                     ticks: {
                         fontSize: 15,
-                        fontColor: "rgba(255,255,255,1)"
+                        // fontColor: "rgba(255,255,255,1)"
                     }
                 }],
                 xAxes: [{
-                    gridLines: {
-                        display: false
-                    },
+                    // gridLines: {
+                    //     display: true
+                    // },
                     ticks: {
                         fontSize: 15,
-                        fontColor: "rgba(255,255,255,1)"
+                        // fontColor: "rgba(255,255,255,1)"
                     }
                 }]
             }
@@ -84,12 +84,14 @@
             let measurementArray = measurement.split(',')
             let date = new Date(Number(measurementArray[0]) * 1000);
             let time = `${date.getHours()}:${('0'+date.getMinutes()).slice(-2)}`
+
             chartCollection['linechart-va'].data.datasets[zone - 1].data.push(measurementArray[1])
             chartCollection['linechart-va'].data.labels.push(time);
             chartCollection['linechart-temp'].data.datasets[zone - 1].data.push(measurementArray[2])
             chartCollection['linechart-temp'].data.labels.push(time);
             chartCollection['linechart-fuel'].data.datasets[zone - 1].data.push(measurementArray[3])
             chartCollection['linechart-fuel'].data.labels.push(time);
+
             chartCollection['linechart-va'].update();
             chartCollection['linechart-temp'].update();
             chartCollection['linechart-fuel'].update();
@@ -107,10 +109,20 @@
 
 var graphButtons = document.querySelectorAll(".graphButtons > button");
 var charts = document.querySelectorAll("canvas");
+var generatorSelection = document.getElementById("selectGenerator")
 
+
+generatorSelection.addEventListener("change", function(graphs){
+        charts.forEach(function(element) {
+            element.style.visibility = "hidden";
+            setTimeout(function(){
+                console.log("Show now")
+                element.style.visibility = "visible";
+            }, 300);
+        });
+});
 
 hideCharts("linechart-va");
-
 graphButtons.forEach(function(element) {
     element.addEventListener("click", function(activeID){
         activeID = element
@@ -123,8 +135,6 @@ graphButtons.forEach(function(element) {
 });
 
 function hideCharts(activeID){
-    console.log(activeID)
-
     charts.forEach(function(chartElement) {
         if (activeID == chartElement.id){
             chartElement.style.display = "block";
@@ -135,3 +145,73 @@ function hideCharts(activeID){
     });
 }
 })();
+
+
+
+
+Highcharts.setOptions({
+    global: {
+        useUTC: false
+    }
+});
+
+// Create the chart
+Highcharts.stockChart('container', {
+    chart: {
+        events: {
+            load: function () {
+
+                // set up the updating of the chart each second
+                var series = this.series[0];
+                setInterval(function () {
+                    var x = (new Date()).getTime(), // current time
+                        y = Math.round(Math.random() * 100);
+                    series.addPoint([x, y], true, true);
+                }, 1000);
+            }
+        }
+    },
+
+    rangeSelector: {
+        buttons: [{
+            count: 1,
+            type: 'minute',
+            text: '1M'
+        }, {
+            count: 5,
+            type: 'minute',
+            text: '5M'
+        }, {
+            type: 'all',
+            text: 'All'
+        }],
+        inputEnabled: false,
+        selected: 0
+    },
+
+    title: {
+        text: 'Live random data'
+    },
+
+    exporting: {
+        enabled: false
+    },
+
+    series: [{
+        name: 'Random data',
+        data: (function () {
+            // generate an array of random data
+            var data = [],
+                time = (new Date()).getTime(),
+                i;
+
+            for (i = -999; i <= 0; i += 1) {
+                data.push([
+                    time + i * 1000,
+                    Math.round(Math.random() * 100)
+                ]);
+            }
+            return data;
+        }())
+    }]
+});
