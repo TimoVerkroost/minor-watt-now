1f4922f434ff7b525566c710d83137b490177303
ColinDorr <colin.maloud@gmail.com>
2017-05-10 12:48:52 +0200

    removed unused code and added some comments

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -107,111 +107,41 @@
         })
     }
 
-var graphButtons = document.querySelectorAll(".graphButtons > button");
-var charts = document.querySelectorAll("canvas");
-var generatorSelection = document.getElementById("selectGenerator")
+    // GRAPH DISPLAY AND BUTTONS
+    var graphButtons = document.querySelectorAll(".graphButtons > button");
+    var charts = document.querySelectorAll("canvas");
+    var generatorSelection = document.getElementById("selectGenerator")
 
+    generatorSelection.addEventListener("change", function(graphs){ // Add eventListener to select, to make it blink once.
+            charts.forEach(function(element) {
+                element.style.visibility = "hidden";
+                setTimeout(function(){
+                    console.log("Show now")
+                    element.style.visibility = "visible";
+                }, 300);
+            });
+    });
 
-generatorSelection.addEventListener("change", function(graphs){
-        charts.forEach(function(element) {
-            element.style.visibility = "hidden";
-            setTimeout(function(){
-                console.log("Show now")
-                element.style.visibility = "visible";
-            }, 300);
+    hideCharts("linechart-va");
+    graphButtons.forEach(function(element) {    // Add eventlisteners, to buttons, to keep track of the active button.
+        element.addEventListener("click", function(activeID){
+            activeID = element
+            graphButtons.forEach(function(graphButtonsElement) {
+                graphButtonsElement.classList.remove("active");
+                activeID.classList.add("active");
+            })
+            hideCharts(activeID.id.slice(7, activeID.id.length));
         });
-});
-
-hideCharts("linechart-va");
-graphButtons.forEach(function(element) {
-    element.addEventListener("click", function(activeID){
-        activeID = element
-        graphButtons.forEach(function(graphButtonsElement) {
-            graphButtonsElement.classList.remove("active");
-            activeID.classList.add("active");
-        })
-        hideCharts(activeID.id.slice(7, activeID.id.length));
     });
-});
 
-function hideCharts(activeID){
-    charts.forEach(function(chartElement) {
-        if (activeID == chartElement.id){
-            chartElement.style.display = "block";
-        }
-        else {
-            chartElement.style.display = "none";
-        }
-    });
-}
-})();
-
-
-
-
-Highcharts.setOptions({
-    global: {
-        useUTC: false
+    function hideCharts(activeID){  // Depending on the active button, show the corresponding chart.
+        charts.forEach(function(chartElement) {
+            if (activeID == chartElement.id){
+                chartElement.style.display = "block";
+            }
+            else {
+                chartElement.style.display = "none";
+            }
+        });
     }
-});
-
-// Create the chart
-Highcharts.stockChart('container', {
-    chart: {
-        events: {
-            load: function () {
-
-                // set up the updating of the chart each second
-                var series = this.series[0];
-                setInterval(function () {
-                    var x = (new Date()).getTime(), // current time
-                        y = Math.round(Math.random() * 100);
-                    series.addPoint([x, y], true, true);
-                }, 1000);
-            }
-        }
-    },
-
-    rangeSelector: {
-        buttons: [{
-            count: 1,
-            type: 'minute',
-            text: '1M'
-        }, {
-            count: 5,
-            type: 'minute',
-            text: '5M'
-        }, {
-            type: 'all',
-            text: 'All'
-        }],
-        inputEnabled: false,
-        selected: 0
-    },
-
-    title: {
-        text: 'Live random data'
-    },
-
-    exporting: {
-        enabled: false
-    },
-
-    series: [{
-        name: 'Random data',
-        data: (function () {
-            // generate an array of random data
-            var data = [],
-                time = (new Date()).getTime(),
-                i;
-
-            for (i = -999; i <= 0; i += 1) {
-                data.push([
-                    time + i * 1000,
-                    Math.round(Math.random() * 100)
-                ]);
-            }
-            return data;
-        }())
-    }]
-});
+})();
