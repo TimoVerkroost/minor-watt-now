9f4598ebfd2b3958200d75b43efec3260532ef1d
Diego Staphorst <diego-s-@hotmail.com>
2017-05-11 14:01:49 +0200

    create new data files, chart and emitter

--- controller/fileReader.js
+++ controller/fileReader.js
@@ -25,35 +25,72 @@ module.exports = class File {
         this.lastFuelData.push(Number(dataArray[3]));
         // RealPower log
         let realPower = Number(dataArray[1]);
-        if(realPower >= 70000 && realPower !== this.lastRealPowerValue) {
-          this.lastRealPowerValue = realPower;
-          pushMessages('Generator', messages.peak.message, messages.peak.priority);
+        if (realPower >= 70000 && realPower !== this.lastRealPowerValue) {
+            this.lastRealPowerValue = realPower;
+            // pushMessages('Generator', messages.peak.message, messages.peak.priority);
         }
         // ApparentPower log
         let apparentPower = Number(dataArray[2]);
-        if(apparentPower <= 5000 && apparentPower !== this.lastApparentPowerValue) {
-          this.lastApparentPowerValue = apparentPower;
-          pushMessages('Generator', messages.efficientLow.message, messages.efficientLow.priority);
+        if (apparentPower <= 5000 && apparentPower !== this.lastApparentPowerValue) {
+            this.lastApparentPowerValue = apparentPower;
+            // pushMessages('Generator', messages.efficientLow.message, messages.efficientLow.priority);
         }
         // Fuel log
         let fuel = Number(dataArray[3]);
-        if(fuel <= 20 && fuel !== this.lastFuelRateValue) {
-          this.lastFuelRateValue = fuel;
-          pushMessages('Generator', messages.fuelLow.message, messages.fuelLow.priority);
+        if (fuel <= 20 && fuel !== this.lastFuelRateValue) {
+            this.lastFuelRateValue = fuel;
+            // pushMessages('Generator', messages.fuelLow.message, messages.fuelLow.priority);
         }
     }
 
     getBackUp() {
         return [this.lastLabels,
-                this.lastRealPowerData,
-                this.lastApparentPowerData,
-                this.lastFuelData];
+            this.lastRealPowerData,
+            this.lastApparentPowerData,
+            this.lastFuelData
+        ];
     }
 
     setFile(path) {
         this.file = require('readline').createInterface({
             input: require('fs').createReadStream(path)
         });
+    }
+
+    emitPhaseLines() {
+        let phase1 = new LineByLineReader('./data/real-data/phase-1-data.csv');
+        let phase2 = new LineByLineReader('./data/real-data/phase-2-data.csv');
+        let phase3 = new LineByLineReader('./data/real-data/phase-3-data.csv');
+        let phaseTotal = new LineByLineReader('./data/real-data/phase-33-data.csv');
+        phase1.on('line', (line) => {
+            phase1.pause();
+            setTimeout(() => {
+                console.log(line)
+                this.socket.emit('addPhase1', line)
+                phase1.resume();
+            }, 1000);
+        })
+        phase2.on('line', (line) => {
+            phase2.pause();
+            setTimeout(() => {
+                this.socket.emit('addPhase2', line)
+                phase2.resume();
+            }, 1000);
+        })
+        phase3.on('line', (line) => {
+            phase3.pause();
+            setTimeout(() => {
+                this.socket.emit('addPhase3', line)
+                phase3.resume();
+            }, 1000);
+        })
+        phaseTotal.on('line', (line) => {
+            phaseTotal.pause();
+            setTimeout(() => {
+                this.socket.emit('addPhase4', line)
+                phaseTotal.resume();
+            }, 1000);
+        })
     }
 
     emitLines() {
